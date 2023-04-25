821aadf0dfee2cb479ebaf540a12858238fb9fe8
APHEFSDSHEAT <1517652@edu.burnabyschools.ca>
2023-04-24 17:36:54 -0700

    April 23
    New sprites, more effects, more sprites...

--- Angry Birds Remake Solo Game/Assets/Scripts/Shredder.cs
+++ Angry Birds Remake Solo Game/Assets/Scripts/Shredder.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Shredder : MonoBehaviour
 {
+    [SerializeField] float durationOfExplosion = 1f;
+    [SerializeField] GameObject explosionVFX;
     private void OnTriggerEnter2D(Collider2D other)
     {
         Destroy(other.gameObject);
+        GameObject explosion = Instantiate(explosionVFX, transform.position, transform.rotation);
+        Destroy(explosion, durationOfExplosion);
     }
 }
